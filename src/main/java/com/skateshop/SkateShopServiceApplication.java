1e0cd08b1dc9fbc22183c0cf077fc2f260254d5d
Matheus Dias <telesmatheusdias@gmail.com>
2026-07-15 13:33:51 -0300

    init commit

---
+++ src/main/java/com/skateshop/SkateShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skateshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateShopServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skateshop/SkateShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skateshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateShopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
